79173724c4ab0d64e2141a4af4e68fa71c419e99
johntarus <tarusjohn96@gmail.com>
2022-03-26 01:41:34 +0300

    setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,7 +30,7 @@ const Home: NextPage = () => {
             href="https://nextjs.org/docs"
             className="mt-6 w-96 rounded-xl border p-6 text-left hover:text-blue-600 focus:text-blue-600"
           >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
+            <h3 className="text-2xl font-bold">Documentation! &rarr;</h3>
             <p className="mt-4 text-xl">
               Find in-depth information about Next.js features and API.
             </p>
