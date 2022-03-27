a2a32bba679b0709aa522fe9ef08ad00dee43de9
johntarus <tarusjohn96@gmail.com>
2022-03-27 11:14:45 +0300

    sett up

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
         <h1 className="text-6xl font-bold">
           Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+            Next.js!!
           </a>
         </h1>
 
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
           >
             <h3 className="text-2xl font-bold">Learn &rarr;</h3>
             <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+              Learn about Next.js in an interactive course with quizzes!!
             </p>
           </a>
 
