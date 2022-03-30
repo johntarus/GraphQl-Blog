40a14dcac1d70a4ae846a0273df1d4f7a1aa8ac5
johntarus <tarusjohn96@gmail.com>
2022-03-30 15:33:57 +0300

    categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Categories = () => {
-  return <div>Categories</div>
+  return (
+    <div className="shaddow-lg mb-8 rounded-lg bg-white p-8">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
+    </div>
+  )
 }
 
 export default Categories
