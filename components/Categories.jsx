831ffc399ee6be59d1ddc61e8a9b32cc9eebd058
johntarus <tarusjohn96@gmail.com>
2022-03-30 16:56:22 +0300

    categories

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Link from 'next/link'
+
+import { getCategories } from '../services'
 
 const Categories = () => {
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories)
+    })
+  }, [])
+
+  console.log(categories)
   return (
-    <div className="shaddow-lg mb-8 rounded-lg bg-white p-8">
+    <div className="shaddow-lg mb-8 rounded-lg bg-white p-8 pb-12">
       <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={'/category/${category.slug}'}>
+          <span className="mb-3 block cursor-pointer pb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
     </div>
   )
 }
