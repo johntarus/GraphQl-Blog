24ff016b4fdaace0d8247760b8ad7225ece8cf20
johntarus <tarusjohn96@gmail.com>
2022-04-03 23:03:34 +0300

    postDetails

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,5 +1,12 @@
 import React from 'react'
 import { getPosts, getPostsDetails } from '../../services'
+import {
+  PostDetails,
+  Categories,
+  PostWidget,
+  Comments,
+  CommentsForm,
+} from '../../components'
 
 const PostDetails = () => {
   return <div></div>
