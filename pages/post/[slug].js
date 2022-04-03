5c4f21c267efe841b3c3acc2143ab4094ccc59d8
johntarus <tarusjohn96@gmail.com>
2022-04-03 23:41:39 +0300

    post

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -11,7 +11,17 @@ import {
 const postDetails = () => {
   return (
     <div className="container mx-auto mb-8 px-10">
-      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12"></div>
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+        <div className="col-span-1 lg:col-span-8">
+          <PostDetails />
+          <Author />
+          <CommentsForm />
+          <Comments />
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky"></div>
+        </div>
+      </div>
     </div>
   )
 }
