a5dcaa15abd2304d89d21c4839cf4bfde4c07ae3
johntarus <tarusjohn96@gmail.com>
2022-04-03 23:23:40 +0300

    posts

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -9,7 +9,11 @@ import {
 } from '../../components'
 
 const postDetails = () => {
-  return <div className="container mx-auto mb-8 px-10">postDetails</div>
+  return (
+    <div className="container mx-auto mb-8 px-10">
+      <div className="grid grid-cols-1 gap-12 lg:grid-cols-12"></div>
+    </div>
+  )
 }
 
 export default postDetails
