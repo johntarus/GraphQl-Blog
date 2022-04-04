a8792fb2d56dc7b06c0ec0dc3a34f4e407cd7d74
johntarus <tarusjohn96@gmail.com>
2022-04-04 23:20:12 +0300

    post details

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const PostDetail = () => {
-  return <div>PostDetail</div>
+  return (
+    <div className="mb-8 rounded-lg bg-white p-8 pb-10 shadow-lg">
+      PostDetail
+    </div>
+  )
 }
 
 export default PostDetail
