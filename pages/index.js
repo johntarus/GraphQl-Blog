d110813ba5a94f3a60867ba53f58c23e369daa46
johntarus <tarusjohn96@gmail.com>
2022-04-04 23:24:57 +0300

    commit

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,6 @@ import Head from 'next/head'
 import { Categories, PostCard, PostWidget, Header } from '../components'
 import { getPosts } from '../services'
 
-// const posts = [
-//   { title: 'React Redux', excerpt: 'React with Redux' },
-//   { title: 'React & Tailwind', excerpt: 'React with Tailwind' },
-// ]
-
 export default function Home({ posts }) {
   return (
     <div className="container mx-auto mb-8 px-10">
