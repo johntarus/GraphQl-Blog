ba1ee88352c8366cd2c4ac98b4ad9039ca32dda9
johntarus <tarusjohn96@gmail.com>
2022-04-06 12:07:20 +0300

    author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 
-const Author = () => {
-  return <div>Author</div>
+const Author = ({ author }) => {
+  return (
+    <img
+      alt={author.name}
+      height="100px"
+      width="100px"
+      className="rounded-full align-middle"
+      src={author.url}
+    />
+  )
 }
 
 export default Author
