483afa4d1e00b73fa243c4e11a8f6fe0a64ed174
johntarus <tarusjohn96@gmail.com>
2022-04-06 12:27:45 +0300

    author

--- components/Author.jsx
+++ components/Author.jsx
@@ -2,13 +2,20 @@ import React from 'react'
 
 const Author = ({ author }) => {
   return (
-    <img
-      alt={author.name}
-      height="100px"
-      width="100px"
-      className="rounded-full align-middle"
-      src={author.url}
-    />
+    <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
+      <div className="absolute left-0 right-2 -top-14">
+        <img
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="rounded-full align-middle"
+          src={author.photo.url}
+        />
+        <h3 className="text-white" my-4 text-bold text-xl>
+          {author.name}
+        </h3>
+      </div>
+    </div>
   )
 }
 
