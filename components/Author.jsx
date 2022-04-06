1ed054de2e3921c307f707fe6ac55487a21f05e2
johntarus <tarusjohn96@gmail.com>
2022-04-06 13:02:26 +0300

    author

--- components/Author.jsx
+++ components/Author.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Author = ({ author }) => {
   return (
     <div className="relative mt-20 mb-8 rounded-lg bg-black bg-opacity-20 p-12 text-center">
-      <div className="absolute left-0 right-2 -top-14">
-        <img
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
           alt={author.name}
+          unoptimized
           height="100px"
           width="100px"
           className="rounded-full align-middle"
           src={author.photo.url}
         />
-        <h3 className="text-white" my-4 text-bold text-xl>
-          {author.name}
-        </h3>
       </div>
+      <h3 className="mt-4 text-xl font-bold text-white text-white text-white">
+        {author.name}
+      </h3>
+      <p className="text-lg text-white">{author.bio}</p>
     </div>
   )
 }
