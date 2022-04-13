51f6465c9078ddfe604220795a2a53b4a98c9c8d
johntarus <tarusjohn96@gmail.com>
2022-04-13 22:19:29 +0300

    comments

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -5,7 +5,7 @@ const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 export default function comments(req, res) {
   const graphqlClient = new GraphQLClient(graphqlAPI, {
     headers: {
-      Authorization: `Bearer ${process.env}`,
+      Authorization: `Bearer ${process.env.GRAPHCMS_API_TOKEN}`,
     },
   })
 }
