fdf19feef12081ce146aff500f140e6cc0a9fb81
johntarus <tarusjohn96@gmail.com>
2022-04-13 22:49:03 +0300

    comments

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -3,6 +3,7 @@ import { GraphQLClient, gql } from 'graphql'
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT
 
 export default function comments(req, res) {
+    const {name, email, comment, slug} = req.body
   const graphqlClient = new GraphQLClient(graphqlAPI, {
     headers: {
       Authorization: `Bearer ${process.env.GRAPHCMS_API_TOKEN}`,
@@ -10,6 +11,10 @@ export default function comments(req, res) {
   })
   const query = gql`
     mutation CreateComment($name: string!, $email: string!, $comment: string!, $slug: string!) {
+        createComment(input: { name: $name, email: $email, comment: $comment, post: slug: {connect {$slug }}}) {id}
     }
     `
+    const result = await graphqlClient.request(query, req.body)
+    return res.status(200).send(result)
+    
 }
