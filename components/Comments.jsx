7f1d9368dbbc061e51ed211b9c2ed7e25b4fa4bb
johntarus <tarusjohn96@gmail.com>
2022-04-14 14:57:08 +0300

    comments

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from 'react'
 import moment from 'moment'
 import parse from 'html-react-parser'
 
-const Comments = () => {
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([])
+  useEffect(() => {
+    // getComments()
+  }, [])
   return <div>Comments</div>
 }
 
