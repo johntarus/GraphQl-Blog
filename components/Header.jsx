b80d7d4cd174af63fd28bb588890a2eb14ea1a55
johntarus <tarusjohn96@gmail.com>
2022-04-25 00:20:17 +0300

    responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -14,12 +14,28 @@ const Header = () => {
   return (
     <div className="container mx-auto mb-8 px-10">
       <div className="inline-block w-full border-b border-blue-400 py-3">
-        <div className="block md:float-left">
+        <div className="block flex items-center justify-between md:float-left">
           <Link href="/">
             <span className="cussor-pointer text-xl font-bold text-white sm:text-4xl">
               <Link href="/">GraphCMS Blog</Link>
             </span>
           </Link>
+          <div className="lg:invisible">
+            <svg
+              class="h-6 w-6"
+              fill="none"
+              stroke="white"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </div>
         </div>
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
